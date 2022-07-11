4dd5190b14d62aa29389c0addbc0341235f1188a
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-07-11 09:38:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var grace = "";
 var scarynight = "";
+leftY = 0;
+leftX = 0;
+rightY = 0;
+rightX = 0;
 
 
 function preload(){
@@ -17,9 +21,37 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
     
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
     }
-    
-    function draw(){
+
+
+function modelLoaded(){
+
+    console.log("wow such load");
+
+}
+
+function gotPoses(results){
+
+if(results.length > 0){
+
+console.log(results);
+leftY = results[0].pose.leftWrist.y;
+leftX = results[0].pose.leftWrist.x;
+rightY = results[0].pose.rightWrist.y;
+rightX = results[0].pose.rightWrist.x;
+
+console.log("left y = " + leftY  + " left x = " + leftX);
+console.log(" right x = " + rightX + " right y = " + rightY)
+
+}
+
+}
+   
+
+function draw(){
     
     image(video, 0, 0, 600, 500);
     
